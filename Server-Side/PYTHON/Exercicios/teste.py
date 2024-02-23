03d167e76eaae0f05841ae0e9b3ad86072ebd6b9
phznn027 <131038276+phznn027@users.noreply.github.com>
2024-02-23 10:27:14 +0000

    Pending changes exported from your codespace

---
+++ Server-Side/PYTHON/Exercicios/teste.py
@@ -0,0 +1,11 @@
+
+
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro numero: '))
+
+n3 = float(input('Digite um numero: '))
+n4 = float(input('Digite outro numero: '))
+
+print(n1/n2)
+
+print(n3/n4)
