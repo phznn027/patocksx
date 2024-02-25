14037b302398d47ce095f8ef14addafb88ba8a0c
phznn027 <131038276+phznn027@users.noreply.github.com>
2024-02-25 06:00:54 +0000

    commit

---
+++ Client-Side/CSS/Exercicios/Ex01/scripts.js
@@ -0,0 +1,18 @@
+/* div = document.getElementsByTagName('div')[1]
+div.style.display = 'none'
+
+div = document.getElementsByTagName('div')[2]
+div.style.display = 'none'
+
+div = document.getElementsByTagName('div')[3]
+div.style.display = 'none'
+
+div = document.getElementsByTagName('div')[4]
+div.style.display = 'none'
+
+div = document.getElementsByTagName('div')[5]
+div.style.display = 'none'
+
+div = document.getElementsByTagName('div')[0]
+div.style.display = 'none'
+*/
