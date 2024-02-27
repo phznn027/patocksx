3c6db05a2c6448d497983010615870507a7fa677
phznn027 <131038276+phznn027@users.noreply.github.com>
2024-02-27 11:33:51 +0000

    commit

--- Server-Side/PYTHON/Projetos/Funcao-2grau/main.py
+++ Server-Side/PYTHON/Projetos/Funcao-2grau/main.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+'''import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(-10, 10, 100)
@@ -21,4 +21,25 @@ plt.axvline(0, color='black',linewidth=0.5)
 plt.legend()
 
 plt.savefig('teste3.png')
+plt.show()
+'''
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-2, 2, 100)
+y = -x + 2
+
+plt.figure(figsize=(8, 6))
+plt.plot(x, y, label='y = -x + 2', color='blue')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Gráfico da função y = -x + 2')
+plt.xticks(np.arange(-2, 3, 1))
+plt.yticks(np.arange(-2, 5, 1))
+plt.axhline(0, color='black',linewidth=0.5)
+plt.axvline(0, color='black',linewidth=0.5)
+plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
+plt.legend()
+plt.savefig('testee.png')
 plt.show()
