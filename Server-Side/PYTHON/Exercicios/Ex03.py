08238af4476bd93795070def7136c10d8e10b5d6
phznn027 <131038276+phznn027@users.noreply.github.com>
2024-03-02 01:22:53 +0000

    commit

--- Server-Side/PYTHON/Exercicios/Ex03.py
+++ Server-Side/PYTHON/Exercicios/Ex03.py
@@ -1,9 +1,11 @@
 velocidade = int(input('Qual e a velocidade do carro: '))
 velocidade_max = 80
+multa = (velocidade - velocidade_max) * 7
 
 if velocidade > velocidade_max:
-    print(f'Voce sera multado pois passou do limite de 80km/h, voce esta a {velocidade}Km/h')
+    print(f'Voce levou uma multado de R${multa}reais\nPois passou do limite de 80km/h, Voce esta a {velocidade}Km/h')
 elif velocidade == velocidade_max:
-    print(f'Voce esta no limite da velocidade maxima, mas o gerente esta maluco e decidiu nao te multa dessa vez!')
+    print(f'Voce esta no limite da velocidade maxima de 80km/h, mas o gerente esta maluco e decidiu nao te multa dessa vez!')
 else:
     print(f'voce esta abaixo do limite maximo parabens, voce esta a {velocidade}km/h')
+    print('Gracas a isso voce ganhou um bonus de 5pontos na Detran-Br')
