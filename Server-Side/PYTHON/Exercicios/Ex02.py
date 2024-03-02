70c8a49e48e9826b1965d3fb23a44ae83c9409a6
phznn027 <131038276+phznn027@users.noreply.github.com>
2024-03-02 01:00:48 +0000

    commit

--- Server-Side/PYTHON/Exercicios/Ex02.py
+++ Server-Side/PYTHON/Exercicios/Ex02.py
@@ -1,18 +1,21 @@
 import random 
 import time 
 
-lista_numeros = ['0', '1', '2', '3', '4', '5']
 
-numero_aleatorio = int(random.choice(lista_numeros))
+#lista_numeros = ['0', '1', '2', '3', '4', '5'] foma errada
+
+#numero_aleatorio = int(random.choice(lista_numeros)) forma errada
+
+computador = random.randint(0,5)
 
 print('Aguarde...\nPensando em um numero!')
 time.sleep(1)
-adivinha = int(input('Tente adivinha qual o numero que o computador pensou?'))
+jogador = int(input('Tente adivinha qual o numero que o computador pensou?'))
 print('PROCESSANDO')
 time.sleep(1)
 
 
-if adivinha == numero_aleatorio:
-    print(f'Voce acertou, pois o numero que o computador pensou foi {numero_aleatorio}')
+if jogador == computador:
+    print(f'Voce acertou, pois o numero que o computador pensou foi {computador}')
 else:
-    print(f'Voce errou, pois o numero que o computador pensou foi {numero_aleatorio}')
+    print(f'Voce errou, pois o numero que o computador pensou foi {computador}')
