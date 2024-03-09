413b4447b45bcf6bb94e491dfcad2d193a65509d
phznn027 <131038276+phznn027@users.noreply.github.com>
2024-03-09 23:07:07 +0000

    commit

--- Server-Side/PYTHON/Exercicios/Ex11.py
+++ Server-Side/PYTHON/Exercicios/Ex11.py
@@ -7,7 +7,7 @@ print(f'O seu imc é {imc:.1f}')
 
 if imc < 18.5:
     print('Voce esta abaixo do peso!.')
-    print('Comece a comer mais!!!.')
+    print('Começar a comer mais!!!.')
 
 elif imc >= 18.5 and imc < 25:
     #elif 18.5 <= imc < 25:
@@ -16,13 +16,13 @@ elif imc >= 18.5 and imc < 25:
 
 elif imc >= 25 and imc < 30:
     print('Voce esta acima do peso!!.')
-    print('Pare de comer retardado(a)!.')
+    print('Pare de comer, esta na hora de fazer uma dieta.')
 
 elif imc >= 30 and imc < 40:
     print('Voce esta obso.')
-    print('Bora fazer uma dieta ne pai? madeira esta caro!.')
+    print('Bora fazer uma dieta ne pai? madeira pro caixao esta caro!.')
 
 elif imc >= 40:
     print('Voce agora esta com obsidade morbida!.')
     print('Se fudeu avisei para parar de comer, e fazer uma dieta!.')
-    print('Agora so vai da despesar com caixao, fdp')
+    print('Agora so vai da despesar com caixao, fdp#@#&@!.')
