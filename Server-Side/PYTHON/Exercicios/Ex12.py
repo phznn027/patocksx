be7dcebd422863dd211346e91016bab1f1264bda
phznn027 <131038276+phznn027@users.noreply.github.com>
2024-03-10 00:14:16 +0000

    commit

--- Server-Side/PYTHON/Exercicios/Ex12.py
+++ Server-Side/PYTHON/Exercicios/Ex12.py
@@ -11,51 +11,67 @@ print('''ESCOLHA UMA DAS OPCOES ABAIXO!
 opcao_jogador = int(input('Informe qual voce quer jogar: '))
 opcao_computador = random.randint(1,3)
 
-print(opcao_computador)
-      
+print('JO')
+time.sleep(1)
+print('KEN')
+time.sleep(0.5)
+print('PO')
+
 if opcao_jogador == 1:
+    print('-=-' * 20)
     print('VOCE ESCOLHEU: PEDRA')
 
     if opcao_computador == 1:
         print('COMPUTADOR ESCOLHEU: PEDRA')
+        print('-=-' * 20)
         print('PARTIDA: EMPATE!')
 
     elif opcao_computador == 2:
         print('COMPUTADOR ESCOLHEU: PAPEL')
+        print('-=-' * 20)
         print('PARTIDA: COMPUTADOR GANHOU!')
 
     elif opcao_computador == 3:
         print('COMPUTADOR ESCOLHEU: TESOURA')
+        print('-=-' * 20)
         print('PARTIDA: VOCE GANHOU!')
 
 elif opcao_jogador == 2:
+    print('-=-' * 20)
     print('VOCE ESCONHEU: PAPEL')
 
     if opcao_computador == 1:
         print('COMPUTADOR ESCOLHEU: PEDRA')
+        print('-=-' * 20)
         print('PARTIDA: VOCE GANHOU!')
 
     elif opcao_computador == 2:
         print('COMPUTADOR ESCOLHEU: PAPEL')
+        print('-=-' * 20)
         print('PARTIDA: EMPATE!')
 
     elif opcao_computador == 3:
         print('COMPUTADOR ESCOLHEU: TESOURA')
+        print('-=-' * 20)
         print('PARTIDA: COMPUTADOR GANHOU!')
 
 elif opcao_jogador == 3:
+    print('-=-' * 20)
     print('VOCE ESCONHEU: TESOURA')
     
     if opcao_computador == 1:
         print('COMPUTADOR ESCOLHEU: PEDRA')
+        print('-=-' * 20)
         print('PARTIDA: COMPUTADOR GANHOU!')
 
     elif opcao_computador == 2:
         print('COMPUTADOR ESCOLHEU: PAPEL')
+        print('-=-' * 20)
         print('PARIDA: VOCE GANHOU!')
 
     elif opcao_computador == 3: 
         print('COMPUTADOR ESCOLHEU: TESOURA')
+        print('-=-' * 20)
         print('PARTIDA: EMPATE!')
 
 else: 
