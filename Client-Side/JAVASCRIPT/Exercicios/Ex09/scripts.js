76cc62fd74e3f24f55f28b2d43e95f112f234b72
phznn027 <131038276+phznn027@users.noreply.github.com>
2024-03-10 06:03:10 +0000

    commit

--- Client-Side/JAVASCRIPT/Exercicios/Ex09/scripts.js
+++ Client-Side/JAVASCRIPT/Exercicios/Ex09/scripts.js
@@ -6,11 +6,11 @@ function verificar() {
     if (velocidade > 60) {
         document.querySelector('#resp').innerHTML = '<strong>DETRAN AVISO!</strong> <br>' + `Voce foi <strong>MULTADO</strong> por esta a ${velocidade}Km/h`
     }
-    else if (velocidade === 60) {
-        document.querySelector('#resp').innerHTML = '<strong>DETRAN AVISO!</strong> <br>' + 'Voce esta no limite de 60Km/h, para se <strong>MULTADO</strong>. Tenha mais cuidado'
-    }
     else if (velocidade < 60) {
         document.querySelector('#resp').innerHTML = '<strong>DETRAN AVISO!</strong> <br>' + 'Parabens voce esta abaixo no limite de 60Km/h'
+    }
+    else {
+        document.querySelector('#resp').innerHTML = '<strong>DETRAN AVISO!</strong> <br>' + 'Voce esta no limite de 60Km/h, para se <strong>MULTADO</strong>. Tenha mais cuidado'
     }
 }
 
